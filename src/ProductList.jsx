d6d0e6abcf9b4fdf983878be2728fade68522ca8
Nikolos333 <c.efstratis@gmail.com>
2025-03-26 21:38:22 -0700

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,31 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+    {plantsArray.map((category, index) => (
+    <div key={index}>
+        <h1><div>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                {/*Similarly like the above plant.name show other details like description and cost*/}
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    ))}
 
 
+<var>addedToCart
+function handleAddToCart(params) dispatch(addItem(product));
+  setAddedToCart((prevState) = (
+     ...prevState,
+     [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+   ));
+
+</var>
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
